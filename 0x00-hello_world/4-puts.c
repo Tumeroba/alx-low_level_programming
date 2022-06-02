e93ab270c66d57cf2ef86c1d1def866b5b713d20
Tumeroba <tumeroba@gmail.com>
2022-06-02 11:10:54 -0700

    editted 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <studio.h>
-/*main - Entry point
+#include <stdio.h>
+/** 
+ * main - Entry point
  *
- *Return: Always 0 (success)
+ * Return: Always 0 (success)
  */
-int main (void)
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle);
 	return(0)
-}	
+}
